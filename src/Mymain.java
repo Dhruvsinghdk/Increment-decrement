04c3972c899c4f02d847a7eec80f4350f8b01a21
Dhruvsinghdk <re621006@gmail.com>
2024-08-25 16:14:23 +0530

    Update Mymain.java

--- src/Mymain.java
+++ src/Mymain.java
@@ -3,6 +3,10 @@ public class Mymain {
         int a = 1;
         int b = a++;
         System.out.println(b);
+        int a = 12;
+//        int b = a;
+        System.out.println(--a);
+        System.out.println(a);
 
     }
 }
